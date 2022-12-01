4abb7459befa21585d5def40a4edd018f3b648b9
Kimidsan <kimidsanpeter@gmail.com>
2022-12-01 16:02:46 +0530

    checkout/cart

--- src/containers/CheckOutPage/CheckOutPage.tsx
+++ src/containers/CheckOutPage/CheckOutPage.tsx
@@ -10,7 +10,7 @@ import ButtonPrimary from "shared/Button/ButtonPrimary";
 import NcImage from "shared/NcImage/NcImage";
 import StartRating from "components/StartRating/StartRating";
 import NcModal from "shared/NcModal/NcModal";
-import ModalSelectDate from "components/ModalSelectDate";
+import CustomInupt from "containers/ShoppingCart/components/CustomInupt";
 import moment from "moment";
 import { DateRage } from "components/HeroSearchForm/StaySearchForm";
 import converSelectedDateToString from "utils/converSelectedDateToString";
@@ -33,36 +33,32 @@ const CheckOutPage: FC<CheckOutPageProps> = ({ className = "" }) => {
     guestInfants: 1,
   });
 
-    const [items, setItems] = useState<any>([])
+  const [items, setItems] = useState<any>([])
 
-    const [name, setName] = useState<string>("");
-    const [email, setEmail] = useState<string>("");
-    const [phone, setPhone] = useState<string>("");
-    const [line1, setLine1] = useState<string>("");
-    const [line2, setLine2] = useState<string>("");
-    const [city, setCity] = useState<string>("");
-    const [postal_code, setPostalCode] = useState<string>("");
-    const [countryCode, setCountryCode] = useState<string>("AF");
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [phone, setPhone] = useState<string>("");
+  const [line1, setLine1] = useState<string>("");
+  const [line2, setLine2] = useState<string>("");
+  const [city, setCity] = useState<string>("");
+  const [postal_code, setPostalCode] = useState<string>("");
+  const [countryCode, setCountryCode] = useState<string>("AF");
 
   useEffect(() => {
-  console.log(countryCode)
-  },[countryCode])
+    console.log(countryCode)
+  }, [countryCode])
 
   const renderSidebar = () => {
     return (
-      <div className="w-full flex flex-col sm:rounded-2xl lg:border border-neutral-200 dark:border-neutral-700 space-y-6 sm:space-y-8 px-0 sm:p-6 xl:p-8">
-        <h3 className="text-2xl font-semibold">Price detail</h3>
-        <div className="flex flex-col sm:flex-row sm:items-center">         
-          <div className="flex-shrink-0 w-full sm:w-40">
-            <div className=" aspect-w-4 aspect-h-3 sm:aspect-h-4 rounded-2xl overflow-hidden">
-              <NcImage src="https://images.pexels.com/photos/6373478/pexels-photo-6373478.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+      <div className="flex flex-col max-w-2xl p-1 dark:bg-inherit dark:text-gray-100  sm:rounded-2xl lg:border border-neutral-200 dark:border-neutral-700 space-y-6 sm:space-y-8 px-0 sm:p-6 xl:p-8">
+        <h3 className="text-2xl font-semibold">Summary</h3>
+        <div className="flex flex-col  ">
+          {/* <div className="flex-shrink-0 w-full sm:w-40">
+            <div className="flex-shrink-0 object-cover w-20 h-20 dark:border-transparent  rounded-2xl overflow-hidden">
+              <NcImage src="https://images.pexels.com/photos/6373478/pexels-photo-6373478.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />             
             </div>
-          </div>
-          <div className="py-5 sm:px-5 space-y-3">
+            <div className="py-5 sm:px-5 space-y-3">
             <div>
-              <span className="text-sm text-neutral-500 dark:text-neutral-400 line-clamp-1">
-                Hotel room in Tokyo, Jappan
-              </span>
               <span className="text-base font-medium mt-1 block">
                 The Lounge & Bar
               </span>
@@ -71,8 +67,64 @@ const CheckOutPage: FC<CheckOutPageProps> = ({ className = "" }) => {
               2 beds · 2 baths
             </span>
             <div className="w-10 border-b border-neutral-200  dark:border-neutral-700"></div>
-            <StartRating point={3}/>
+           
           </div>
+          </div> */}
+          <div className="flex w-full space-x-2 sm:space-x-4">
+            <img className="flex-shrink-0 object-cover w-20 h-20 dark:border-transparent rounded outline-none sm:w-20 sm:h-20 dark:bg-gray-500" src="https://images.pexels.com/photos/6373478/pexels-photo-6373478.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Polaroid camera"
+            />
+            <div className="flex flex-col justify-between w-full pb-4">
+              <div className="flex justify-between w-full pb-2 space-x-2">
+                <div className="space-y-1">
+                  <h3 className="text-lg font-semibold leading-snug sm:pr-8">name</h3>
+                  <p className="text-sm dark:text-gray-400">04</p>
+                </div>
+                <div className="text-center">
+                  <p className="text-lg font-semibold">2.33$</p>
+                  {/* <p className="text-sm line-through dark:text-gray-600">75.50€</p> */}
+                </div>
+              </div>
+              <div className="flex text-sm mt-3">
+
+                <div className="flex items-right px-2 py-1 pl-0 space-x-1">
+                  <CustomInupt className="mr-2"  />
+                  {/* defaultValue={item.quantity} onChange={(e) => { editQuantity(item.id, item.type, e); setNewProduct(true); }} */}
+                </div>
+                <button type="button" className="flex items-center px-1 py-1 pl-0 space-x-1 fill-red-600" >
+                {/* onClick={() => { removeCart(item.id, item.type); setNewProduct(true); }} */}
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-3 h-3 ">
+                    <path d="M96,472a23.82,23.82,0,0,0,23.579,24H392.421A23.82,23.82,0,0,0,416,472V152H96Zm32-288H384V464H128Z"></path>
+                    <rect width="32" height="200" x="168" y="216"></rect>
+                    <rect width="32" height="200" x="240" y="216"></rect>
+                    <rect width="32" height="200" x="312" y="216"></rect>
+                    <path d="M328,88V40c0-13.458-9.488-24-21.6-24H205.6C193.488,16,184,26.542,184,40V88H64v32H448V88ZM216,48h80V88H216Z"></path>
+                  </svg>
+                  <span className="text-red-700">Remove</span>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="flex w-full space-x-2 sm:space-x-4 mt-5">
+            <img className="flex-shrink-0 object-cover w-20 h-20 dark:border-transparent rounded outline-none sm:w-20 sm:h-20 dark:bg-gray-500" src="https://images.pexels.com/photos/6373478/pexels-photo-6373478.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Polaroid camera"
+            />
+            <div className="flex flex-col justify-between w-full pb-4">
+              <div className="flex justify-between w-full pb-2 space-x-2">
+                <div className="space-y-1">
+                  <h3 className="text-lg font-semibold leading-snug sm:pr-8">name</h3>
+                  <p className="text-sm dark:text-gray-400">04</p>
+                </div>
+                <div className="text-center">
+                  <p className="text-lg font-semibold">2.33$</p>
+                
+                </div>
+              </div>
+              <div className="flex text-sm ">
+
+               
+              </div>
+            </div>
+          </div>
+
         </div>
         <div className="flex flex-col space-y-4">
           {/* <h3 className="text-2xl font-semibold">Price detail</h3> */}
@@ -96,10 +148,10 @@ const CheckOutPage: FC<CheckOutPageProps> = ({ className = "" }) => {
   };
 
   const renderMain = () => {
-    
+
     return (
       <div className="w-full flex flex-col sm:rounded-2xl sm:border border-neutral-200 dark:border-neutral-700 space-y-8 px-0 sm:p-6 xl:p-8">
-        <h2 className="text-3xl lg:text-4xl font-semibold">
+        <h2 className="text-3xl lg:text-4xl font-semibold mt-11">
           Confirm and Payment
         </h2>
         <div className="border-b border-neutral-200 dark:border-neutral-700"></div>
@@ -108,7 +160,7 @@ const CheckOutPage: FC<CheckOutPageProps> = ({ className = "" }) => {
             <h3 className="text-2xl font-semibold">Shipping Details</h3>
             <div className="w-14 border-b border-neutral-200 dark:border-neutral-700 my-5"></div>
           </div>
-          
+
           <div>
             <div className="space-y-1 mb-4">
               <Label>Recipient Name </Label>
@@ -139,8 +191,8 @@ const CheckOutPage: FC<CheckOutPageProps> = ({ className = "" }) => {
               </div>
               <div className="space-y-1 mb-4">
                 <select onChange={(e) => setCountryCode(e.target.value)}>
-                  {COUNTRIES.map((item:any,key:number)=>{
-                    return[
+                  {COUNTRIES.map((item: any, key: number) => {
+                    return [
                       <option value={item.code}>{item.name}</option>
                     ]
                   })}
@@ -162,8 +214,8 @@ const CheckOutPage: FC<CheckOutPageProps> = ({ className = "" }) => {
                 <Tab as={Fragment}>
                   {({ selected }) => (
                     <button
-                      className={`px-4 py-1.5 sm:px-6 sm:py-2.5  rounded-full flex items-center justify-center focus:outline-none  ${selected? "bg-neutral-800 dark:bg-neutral-300 text-white dark:text-neutral-900"
-                          : " text-neutral-6000 dark:text-neutral-400"
+                      className={`px-4 py-1.5 sm:px-6 sm:py-2.5  rounded-full flex items-center justify-center focus:outline-none  ${selected ? "bg-neutral-800 dark:bg-neutral-300 text-white dark:text-neutral-900"
+                        : " text-neutral-6000 dark:text-neutral-400"
                         }`}
                     >
                       <span className="mr-2.5">Card</span>
@@ -178,8 +230,8 @@ const CheckOutPage: FC<CheckOutPageProps> = ({ className = "" }) => {
                   {({ selected }) => (
                     <button
                       className={`px-4 py-1.5 sm:px-6 sm:py-2.5 rounded-full focus:outline-none ${selected
-                          ? "bg-neutral-800 dark:bg-neutral-300 text-white dark:text-neutral-900"
-                          : "text-neutral-6000 dark:text-neutral-400"
+                        ? "bg-neutral-800 dark:bg-neutral-300 text-white dark:text-neutral-900"
+                        : "text-neutral-6000 dark:text-neutral-400"
                         }`}
                     >
                       Cash On Delivery
@@ -230,7 +282,7 @@ const CheckOutPage: FC<CheckOutPageProps> = ({ className = "" }) => {
     <div className={`nc-CheckOutPage ${className}`} data-nc-id="CheckOutPage">
       <main className="container mt-11 mb-24 lg:mb-32 flex flex-col-reverse lg:flex-row">
         <div className="w-full lg:w-3/5 xl:w-2/3 lg:pr-10 ">{renderMain()}</div>
-        <div className="hidden lg:block flex-grow">{renderSidebar()}</div>
+        <div className=" flex-grow">{renderSidebar()}</div>
       </main>
     </div>
   );
