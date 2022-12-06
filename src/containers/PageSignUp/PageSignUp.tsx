b525666099cd06c63f2c775d1dcbb6c949ab6843
Sayanthiny <rmsayanthi@gmail.com>
2022-12-06 16:58:28 +0530

    new signup

--- src/containers/PageSignUp/PageSignUp.tsx
+++ src/containers/PageSignUp/PageSignUp.tsx
@@ -87,128 +87,124 @@ const PageSignUp: FC<PageSignUpProps> = ({ className = "" }) => {
 
 
   return (
-    <div className={`nc-PageSignUp  ${className}`} data-nc-id="PageSignUp">
-      <Helmet>
-        <title>Sign up || Anytime Eat</title>
-      </Helmet>
-      
+<section className="h-screen">
+  <div className="container px-6 py-12 h-full">
+    <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+      <div className="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
+        <img
+          src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+          className="w-full"
+          alt="Phone image"
+        />
+      </div>
+      <div className="md:w-8/12 lg:w-5/12 lg:ml-20">
+        <form>
+        <div className="mb-6">
+            <input
+              type="text"
+              className="form-control block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              placeholder="Username"
+            />
+          </div>
+          <div className="mb-6">
+            <input
+              type="text"
+              className="form-control block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              placeholder="Email address"
+            />
+          </div>
 
-      <div className="container mb-24 lg:mb-32">
-        <ToastContainer />
-        <h2 className="my-20 flex items-center text-3xl leading-[115%] md:text-5xl md:leading-[115%] font-semibold text-neutral-900 dark:text-neutral-100 justify-center">
-          Signup
-          {/* <img
-            src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/soft-ui-dashboard/assets/img/drake.jpg"
-            alt="avatar image"
-            className="inline-flex items-center justify-center w-12 h-12 mr-2 text-white transition-all duration-200 ease-in-out text-lg rounded-circle "
-          /> */}
-        </h2>
+          <div className="mb-6">
+            <input
+              type="password"
+              className="form-control block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              placeholder="Password"
+            />
+          </div>
 
-        <div className="max-w-md mx-auto space-y-6 ">
-          <div className="grid gap-3">
-            {loginSocials.map((item, index) => (
-              <a
-                key={index}
-                href={item.href}
-                className="nc-will-change-transform flex w-full rounded-lg bg-primary-50 dark:bg-neutral-800 px-4 py-3 transform transition-transform sm:px-6 hover:translate-y-[-2px]"
+          <div className="mb-6">
+            <input
+              type="password"
+              className="form-control block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              placeholder="Confirm Password"
+            />
+          </div>
+
+          <div className="flex justify-between items-center mb-6">
+            <div className="form-group form-check">
+              <input
+                type="checkbox"
+                className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                id="exampleCheck3"
+                checked
+              />
+              <label className="form-check-label inline-block text-gray-800" htmlFor="exampleCheck2"
+                >Remember me</label
               >
-                <img
-                  className="flex-shrink-0"
-                  src={item.icon}
-                  alt={item.name}
-                />
-                <h3 className="flex-grow text-center text-sm font-medium text-neutral-700 dark:text-neutral-300 sm:text-sm">
-                  {item.name}
-                </h3>
-              </a>
-            ))}
+            </div>
+            <a
+              href="#!"
+              className="text-blue-600 hover:text-blue-700 focus:text-blue-700 active:text-blue-800 duration-200 transition ease-in-out"
+              >Forgot password?</a
+            >
           </div>
-          {/* OR */}
-          <div className="relative text-center">
-            <span className="relative z-10 inline-block px-4 font-medium text-sm bg-white dark:text-neutral-400 dark:bg-neutral-900">
-              OR
-            </span>
-            <div className="absolute left-0 w-full top-1/2 transform -translate-y-1/2 border border-neutral-100 dark:border-neutral-800"></div>
+
+          <button
+            type="submit"
+            className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+          >
+            Sign in
+          </button>
+
+          <div
+            className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5"
+          >
+            <p className="text-center font-semibold mx-4 mb-0">OR</p>
           </div>
-          {/* FORM */}
-          <form className="grid grid-cols-1 gap-6" onSubmit={(e) => { e.preventDefault(); handleApi() }}>
-            <label className="block">
-              <Input
-                type="text"
-                placeholder="User Name"
-                className=""
-                onChange={(e) => {
-                  setUsername(e.target.value)
-                }}
-                required={true}
-              />
 
-            </label>
-            <label className="block">
-              <Input
-                type="email"
-                placeholder="example@example.com"
-                className=""
-                onChange={(e) => {
-                  setEmail(e.target.value)
-                }}
-                required={true}
-              />
-            </label>
-            <label className="block">
-              <Input
-                type="text"
-                placeholder="Address"
-                className=""
-                onChange={(e) => {
-                  setAddress(e.target.value)
-                }}
-                
-              />
-            </label>
-            <label className="block">
-              <Input
-                type="text"
-                placeholder="+123456789"
-                className=""
-                onChange={(e) => {
-                  setPhone(e.target.value)
-                }}
-                
-              />
-            </label>
-            <label className="block">
-
-              <Input type="password"
-                placeholder="Password"
-                className=""
-                onChange={(e) => {
-                  setPassword(e.target.value)
-                }}
-                required={true} />
-            </label>
-            <label className="block">
-
-              <Input type="password"
-                placeholder="Confirm Password"
-                className=""
-                onChange={(e) => {
-                  setConfirmPassword(e.target.value)
-                }}
-                required={true} />
-            </label>
-
-            <ButtonPrimary type="submit">Sign Up</ButtonPrimary>
-          </form>
-
-          {/* ==== */}
-          <span className="block text-center text-neutral-700 dark:text-neutral-300">
-            Already have an account? {` `}
-            <Link to="/login">Sign in</Link>
-          </span>
-        </div>
+          <a
+            className="px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3"
+            // style="background-color: #3b5998"
+            href="#!"
+            role="button"
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 320 512"
+              className="w-3.5 h-3.5 mr-2"
+            >
+              <path
+                fill="currentColor"
+                d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"
+              /></svg>Continue with Facebook
+          </a>
+          <a
+            className="px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center"
+            // style="background-color: #55acee"
+            href="#!"
+            role="button"
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              className="w-3.5 h-3.5 mr-2"
+            >
+              <path
+                fill="currentColor"
+                d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"
+              /></svg>Continue with Twitter
+          </a>
+        </form>
       </div>
     </div>
+  </div>
+</section>
   );
 };
 
