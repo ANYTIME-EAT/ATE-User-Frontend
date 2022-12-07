05e3052da3ca7f9c5d8bf4346a90c2982278d0ea
Sayanthiny <rmsayanthi@gmail.com>
2022-12-07 11:34:11 +0530

    new login register

--- src/containers/PageSignUp/PageSignUp.tsx
+++ src/containers/PageSignUp/PageSignUp.tsx
@@ -86,10 +86,12 @@ const PageSignUp: FC<PageSignUpProps> = ({ className = "" }) => {
   }
 
 
-  return (
+return (
 <section className="h-screen">
   <div className="container px-6 py-12 h-full">
+  <ToastContainer />
     <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+    
       <div className="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
         <img
           src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
@@ -98,12 +100,16 @@ const PageSignUp: FC<PageSignUpProps> = ({ className = "" }) => {
         />
       </div>
       <div className="md:w-8/12 lg:w-5/12 lg:ml-20">
-        <form>
+        <form onSubmit={(e) => {e.preventDefault(); handleApi()}}>
         <div className="mb-6">
             <input
               type="text"
               className="form-control block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
               placeholder="Username"
+              onChange={(e) => {
+                setUsername(e.target.value)
+              }}
+              required={true}
             />
           </div>
           <div className="mb-6">
@@ -111,6 +117,37 @@ const PageSignUp: FC<PageSignUpProps> = ({ className = "" }) => {
               type="text"
               className="form-control block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
               placeholder="Email address"
+              onChange={(e) => {
+                setEmail(e.target.value)
+              }}
+              required={true}
+            />
+          </div>
+
+          <div className="mb-6">
+            <input
+              type="text"
+              className="form-control block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-white 
+              bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 
+              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              placeholder="Phone number"
+              onChange={(e) => {
+                setPhone(e.target.value)
+              }}
+            />
+          </div>
+
+          <div className="mb-6">
+            <input
+              type="text"
+              className="form-control block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-white 
+              bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 
+              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              placeholder="Username"
+              onChange={(e) => {
+                setAddress(e.target.value)
+              }}
+           
             />
           </div>
 
@@ -119,6 +156,10 @@ const PageSignUp: FC<PageSignUpProps> = ({ className = "" }) => {
               type="password"
               className="form-control block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
               placeholder="Password"
+              onChange={(e) => {
+                setPassword(e.target.value)
+              }}
+              required={true}
             />
           </div>
 
@@ -127,6 +168,10 @@ const PageSignUp: FC<PageSignUpProps> = ({ className = "" }) => {
               type="password"
               className="form-control block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
               placeholder="Confirm Password"
+              onChange={(e) => {
+                setConfirmPassword(e.target.value)
+              }}
+              required={true}
             />
           </div>
 
@@ -142,31 +187,38 @@ const PageSignUp: FC<PageSignUpProps> = ({ className = "" }) => {
                 >Remember me</label
               >
             </div>
-            <a
+            {/* <a
               href="#!"
               className="text-blue-600 hover:text-blue-700 focus:text-blue-700 active:text-blue-800 duration-200 transition ease-in-out"
               >Forgot password?</a
-            >
+            > */}
           </div>
 
-          <button
+          {/* <button
             type="submit"
             className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
             data-mdb-ripple="true"
             data-mdb-ripple-color="light"
+
           >
-            Sign in
-          </button>
+            Sign up
+          </button> */}
+          <ButtonPrimary type="submit" className="inline-block px-7 py-3 bg-blue-600 text-white font-medium
+           text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 
+           focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg 
+          transition duration-150 ease-in-out w-full">Sign Up</ButtonPrimary>
 
           <div
-            className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5"
+            className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 
+            after:flex-1 after:border-t after:border-gray-300 after:mt-0.5"
           >
             <p className="text-center font-semibold mx-4 mb-0">OR</p>
           </div>
 
           <a
-            className="px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3"
-            // style="background-color: #3b5998"
+            className="px-7 py-3 text-white  bg-blue-900 font-medium text-sm leading-snug uppercase 
+            rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg 
+            transition duration-150 ease-in-out w-full flex justify-center items-center mb-3"
             href="#!"
             role="button"
             data-mdb-ripple="true"
@@ -183,8 +235,9 @@ const PageSignUp: FC<PageSignUpProps> = ({ className = "" }) => {
               /></svg>Continue with Facebook
           </a>
           <a
-            className="px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center"
-            // style="background-color: #55acee"
+            className="px-7 py-3 bg-blue-400 text-white font-medium text-sm leading-snug uppercase rounded shadow-md 
+            hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition 
+            duration-150 ease-in-out w-full flex justify-center items-center"
             href="#!"
             role="button"
             data-mdb-ripple="true"
