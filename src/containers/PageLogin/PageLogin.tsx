806f75cd093d342e6b8106a1736cc54e967bc097
Sayanthiny <rmsayanthi@gmail.com>
2022-12-07 14:43:43 +0530

    login signup changes

--- src/containers/PageLogin/PageLogin.tsx
+++ src/containers/PageLogin/PageLogin.tsx
@@ -86,7 +86,7 @@ const PageLogin: FC<PageLoginProps> = ({ className = "" }) => {
           alt="Sample image"
         />
       </div>
-      <div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
+      <div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0 border p-4 border-red-200 rounded-xl">
         <form onSubmit={(e) => {e.preventDefault(); handleSubmit()}}>
           <div className="flex flex-row items-center justify-center lg:justify-start">
             <p className="text-lg mb-0 mr-4">Sign in with</p>
@@ -94,10 +94,10 @@ const PageLogin: FC<PageLoginProps> = ({ className = "" }) => {
               type="button"
               data-mdb-ripple="true"
               data-mdb-ripple-color="light"
-              className="inline-block p-3 bg-blue-600 text-white 
+              className="inline-block p-3 bg-red-600 text-white 
               font-medium text-xs leading-tight uppercase rounded-full 
-              shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 
-              focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 
+              shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 
+              focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 
               active:shadow-lg transition duration-150 ease-in-out mx-1"
             >
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" className="w-4 h-4">
@@ -112,7 +112,7 @@ const PageLogin: FC<PageLoginProps> = ({ className = "" }) => {
               type="button"
               data-mdb-ripple="true"
               data-mdb-ripple-color="light"
-              className="inline-block p-3 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mx-1"
+              className="inline-block p-3 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out mx-1"
             >
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-4 h-4">
                 <path
@@ -126,7 +126,7 @@ const PageLogin: FC<PageLoginProps> = ({ className = "" }) => {
               type="button"
               data-mdb-ripple="true"
               data-mdb-ripple-color="light"
-              className="inline-block p-3 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mx-1"
+              className="inline-block p-3 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out mx-1"
             >
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="w-4 h-4">
                  <path
@@ -139,8 +139,8 @@ const PageLogin: FC<PageLoginProps> = ({ className = "" }) => {
 
           <div
             className="flex items-center my-4 before:flex-1 
-            before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 
-            after:border-t after:border-gray-300 after:mt-0.5"
+            before:border-t before:border-red-300 before:mt-0.5 after:flex-1 
+            after:border-t after:border-red-300 after:mt-0.5"
           >
             <p className="text-center font-semibold mx-4 mb-0">Or</p>
           </div>
@@ -148,8 +148,8 @@ const PageLogin: FC<PageLoginProps> = ({ className = "" }) => {
             <input
               type="text"
               className="form-control block w-full px-4 py-2 text-sm font-normal 
-              text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 
-              rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 
+              text-gray-700 bg-white bg-clip-padding border border-solid border-red-300 
+              rounded-xl ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-red-600 
               focus:outline-none"
               id="exampleFormControlInput2"
               placeholder="Email address"
@@ -159,7 +159,7 @@ const PageLogin: FC<PageLoginProps> = ({ className = "" }) => {
           <div className="mb-6">
             <input
               type="password"
-              className="form-control block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              className="form-control block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-red-300 rounded-xl ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-red-600 focus:outline-none"
               id="exampleFormControlInput2"
               placeholder="Password"
               onChange={(e) => setPassword(e.target.value)}
@@ -170,7 +170,7 @@ const PageLogin: FC<PageLoginProps> = ({ className = "" }) => {
             <div className="form-group form-check">
               <input
                 type="checkbox"
-                className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                className="form-check-input appearance-none h-4 w-4 border border-red-300 rounded-sm bg-white checked:bg-red-600 checked:border-red-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
                 id="exampleCheck2"
               />
               <label className="form-check-label inline-block text-gray-800 text-sm" htmlFor="exampleCheck2">Remember me</label
@@ -182,16 +182,16 @@ const PageLogin: FC<PageLoginProps> = ({ className = "" }) => {
           <div className="text-center lg:text-left">
             {/* <button
               type="button"
-              className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug 
-              uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 
-              focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg 
+              className="inline-block px-7 py-3 bg-red-600 text-white font-medium text-sm leading-snug 
+              uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 
+              focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg 
               transition duration-150 ease-in-out"
             >
               Login
             </button> */}
-            <ButtonPrimary type="submit" className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug 
-              uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 
-              focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg 
+            <ButtonPrimary type="submit" className="inline-block px-7 py-3 bg-red-600 text-white font-medium text-sm leading-snug 
+              uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 
+              focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg 
               transition duration-150 ease-in-out">Login</ButtonPrimary>
             <p className="text-sm font-semibold mt-2 pt-1 mb-0">
               Don't have an account?
--- src/containers/PageSignUp/PageSignUp.tsx
+++ src/containers/PageSignUp/PageSignUp.tsx
@@ -99,12 +99,14 @@ return (
           alt="Phone image"
         />
       </div>
-      <div className="md:w-8/12 lg:w-5/12 lg:ml-20">
+      <div className="md:w-8/12 lg:w-5/12 lg:ml-20 border p-4 border-red-200 rounded-xl">
         <form onSubmit={(e) => {e.preventDefault(); handleApi()}}>
         <div className="mb-6">
             <input
               type="text"
-              className="form-control block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              className="rounded-xl form-control block w-full px-4 py-2 text-sm font-normal text-gray-700 
+              bg-white bg-clip-padding border border-solid border-red-300 ease-in-out m-0 focus:text-gray-700 
+              focus:bg-white focus:border-red-600 focus:outline-none"
               placeholder="Username"
               onChange={(e) => {
                 setUsername(e.target.value)
@@ -115,7 +117,9 @@ return (
           <div className="mb-6">
             <input
               type="text"
-              className="form-control block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              className="rounded-xl form-control block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-white
+               bg-clip-padding border border-solid border-red-300 ease-in-out m-0 focus:text-gray-700 focus:bg-white 
+               focus:border-red-600 focus:outline-none"
               placeholder="Email address"
               onChange={(e) => {
                 setEmail(e.target.value)
@@ -127,9 +131,9 @@ return (
           <div className="mb-6">
             <input
               type="text"
-              className="form-control block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-white 
-              bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 
-              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              className="rounded-xl form-control block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-white 
+              bg-clip-padding border border-solid border-red-300 ease-in-out m-0 
+              focus:text-gray-700 focus:bg-white focus:border-red-600 focus:outline-none"
               placeholder="Phone number"
               onChange={(e) => {
                 setPhone(e.target.value)
@@ -140,10 +144,10 @@ return (
           <div className="mb-6">
             <input
               type="text"
-              className="form-control block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-white 
-              bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 
-              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-              placeholder="Username"
+              className="rounded-xl form-control block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-white 
+              bg-clip-padding border border-solid border-red-300 ease-in-out m-0 
+              focus:text-gray-700 focus:bg-white focus:border-red-600 focus:outline-none"
+              placeholder="Address"
               onChange={(e) => {
                 setAddress(e.target.value)
               }}
@@ -154,7 +158,9 @@ return (
           <div className="mb-6">
             <input
               type="password"
-              className="form-control block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              className="rounded-xl form-control block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-white 
+              bg-clip-padding border border-solid border-red-300 ease-in-out m-0 focus:text-gray-700 
+              focus:bg-white focus:border-red-600 focus:outline-none"
               placeholder="Password"
               onChange={(e) => {
                 setPassword(e.target.value)
@@ -166,7 +172,9 @@ return (
           <div className="mb-6">
             <input
               type="password"
-              className="form-control block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              className="form-control block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-white bg-clip-padding 
+              border border-solid border-red-300 ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-red-600 
+              focus:outline-none rounded-xl"
               placeholder="Confirm Password"
               onChange={(e) => {
                 setConfirmPassword(e.target.value)
@@ -179,7 +187,10 @@ return (
             <div className="form-group form-check">
               <input
                 type="checkbox"
-                className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                className="form-check-input appearance-none h-4 w-4 
+                border border-gray-300 rounded-sm bg-white checked:bg-red-600 checked:border-red-600 
+                focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain 
+                float-left mr-2 cursor-pointer"
                 id="exampleCheck3"
                 checked
               />
@@ -203,19 +214,19 @@ return (
           >
             Sign up
           </button> */}
-          <ButtonPrimary type="submit" className="inline-block px-7 py-3 bg-blue-600 text-white font-medium
-           text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 
-           focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg 
+          <ButtonPrimary type="submit" className="inline-block px-7 py-3 bg-red-600 text-white font-medium
+           text-sm leading-snug uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 
+           focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg 
           transition duration-150 ease-in-out w-full">Sign Up</ButtonPrimary>
 
-          <div
+          {/* <div
             className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 
             after:flex-1 after:border-t after:border-gray-300 after:mt-0.5"
-          >
-            <p className="text-center font-semibold mx-4 mb-0">OR</p>
-          </div>
+          > */}
+            {/* <p className="text-center font-semibold mx-4 mb-0">OR</p> */}
+          {/* </div> */}
 
-          <a
+          {/* <a
             className="px-7 py-3 text-white  bg-blue-900 font-medium text-sm leading-snug uppercase 
             rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg 
             transition duration-150 ease-in-out w-full flex justify-center items-center mb-3"
@@ -252,7 +263,7 @@ return (
                 fill="currentColor"
                 d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"
               /></svg>Continue with Twitter
-          </a>
+          </a> */}
         </form>
       </div>
     </div>
