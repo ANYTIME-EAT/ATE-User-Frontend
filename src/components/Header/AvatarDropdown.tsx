6ca0bb85f72d31a4d4df4e80c1ee1b649e243243
Sayanthiny <rmsayanthi@gmail.com>
2022-12-19 10:53:40 +0530

    logout changes

--- src/components/Header/AvatarDropdown.tsx
+++ src/components/Header/AvatarDropdown.tsx
@@ -12,28 +12,7 @@ import { Link } from "react-router-dom";
 import Avatar from "shared/Avatar/Avatar";
 import { useNavigate } from "react-router-dom";
 
-const solutions = [
-  {
-    name: "Account",
-    href: "/author",
-    icon: UserCircleIcon,
-  },
-  // {
-  //   name: "Messages",
-  //   href: "##",
-  //   icon: ChatBubbleBottomCenterTextIcon,
-  // },
-  // {
-  //   name: "Wishlists",
-  //   href: "/account-savelists",
-  //   icon: HeartIcon,
-  // },
-  // {
-  //   name: "Booking",
-  //   href: "##",
-  //   icon: HomeIcon,
-  // },
-];
+
 
 const solutionsFoot = [
   {
@@ -99,22 +78,7 @@ export default function AvatarDropdown() {
                     ))}
                   </div>
                   <hr className="h-[1px] border-t border-neutral-300 dark:border-neutral-700" />
-                  <div className="relative grid gap-6 bg-white dark:bg-neutral-800 p-7">
-                    {solutionsFoot.map((item, index) => (
-                      <a
-                        key={index}
-                        href={item.href}
-                        className="flex items-center p-2 -m-3 transition duration-150 ease-in-out rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
-                      >
-                        <div className="flex items-center justify-center flex-shrink-0 text-neutral-500 dark:text-neutral-300">
-                          <item.icon aria-hidden="true" className="w-6 h-6" />
-                        </div>
-                        <div className="ml-4">
-                          <p className="text-sm font-medium ">{item.name}</p>
-                        </div>
-                      </a>
-                    ))}
-                  </div>
+                
                 </div>
               </Popover.Panel>
             </Transition>
