486de688093ec9b3622aad26b470a6b3f96a59cf
Ramachandran_Rajeevaletshanth <vjvfc2k16@gmail.com>
2022-12-21 16:09:40 +0530

    socket.io server change

--- src/containers/Chat/Chat.tsx
+++ src/containers/Chat/Chat.tsx
@@ -2,8 +2,9 @@ import Stepper from "containers/Stepper/Stepper";
 import React, { useState, useEffect } from "react";
 import { getOrderStatusApi } from "../../services/apiServices";
 import io from "socket.io-client";
+import config from '../../config/config.json'
 
-const socket = io("http://localhost:5005");
+const socket = io(config.SERVER_URL);
 
 const Tracking = () => {
   //Chat
